83dfcf0d92ecd94184852e67073f5fbd670ba696
Hasan Ashab <samertask@gmail.com>
2025-08-18 18:25:55 +0600

    backup

--- src/backend/src/test/java/backend/hobbiebackend/HobbieBackendApplicationTests.java
+++ src/backend/src/test/java/backend/hobbiebackend/HobbieBackendApplicationTests.java
@@ -3,6 +3,7 @@ package backend.hobbiebackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HobbieBackendApplicationTests {
     @Test
